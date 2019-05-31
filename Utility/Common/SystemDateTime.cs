65c9eb7f859f2b0e6d2ec2e93bfbd94212469813
VS_Online <maxhank22@gmail.com>
2019-05-31 21:51:31 +0800

    加入專案檔案。

---
+++ Utility/Common/SystemDateTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Common
+{
+    public static class SystemDateTime
+    {
+        public static DateTime ThisSystemDateTime;
+        public static bool IsSet;
+
+        public static void Set(DateTime dateTime)
+        {
+            ThisSystemDateTime = dateTime;
+            IsSet = true;
+        }
+
+        public static DateTime Now()
+        {
+            if (!IsSet)
+                ThisSystemDateTime = DateTime.Now;
+
+            return ThisSystemDateTime;
+        }
+    }
+}
