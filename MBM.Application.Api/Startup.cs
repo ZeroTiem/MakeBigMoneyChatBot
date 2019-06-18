5ccdab01c28f040013d85ad5658adcc0d168a498
Albert <albert.shen@git4u.com>
2019-06-18 16:53:09 +0800

    Swagger document implementation

--- MBM.Application.Api/Startup.cs
+++ MBM.Application.Api/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace MBM.Application.Api
 {
@@ -27,17 +28,33 @@ namespace MBM.Application.Api
         public IConfiguration Configuration { get; }
 
         /// <summary>
-        /// Configures the services.
+        /// Configures the services. (註冊服務)
         /// </summary>
         /// <param name="services">The services.</param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers()
                 .AddNewtonsoftJson();
+
+            // Swagger 產生器是負責取得 API 的規格並產生 SwaggerDocument 物件。
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "API",
+                    Description = "Test API with ASP.NET Core 3.0",
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "Albert",
+                        Email = "albert.shen@gib4u.com",
+                    }
+                });
+            });
         }
 
         /// <summary>
-        /// Configures the specified application.
+        /// Configures the specified application. (註冊 Middleware)
         /// </summary>
         /// <param name="app">The application.</param>
         /// <param name="env">The env.</param>
@@ -68,6 +85,23 @@ namespace MBM.Application.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            // Swagger Middleware 負責路由，提供 SwaggerDocument 物件。
+            app.UseSwagger();
+
+            // SwaggerUI 是負責將 SwaggerDocument 物件變成漂亮的介面。
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(
+                    // url: 需配合 SwaggerDoc 的 name。 "/swagger/{SwaggerDoc name}/swagger.json"
+                    url: "/swagger/v1/swagger.json",
+
+                    // name: 用於 Swagger UI 右上角選擇不同版本的 SwaggerDocument 顯示名稱使用。
+                    name: "Test API V1");
+
+                // 若要在應用程式的根目錄(http://localhost:<port>/) 上提供 Swagger UI，請將 RoutePrefix 屬性設為空字串
+                c.RoutePrefix = string.Empty;
             });
         }
     }
